bad0627771f3a117d86ae59c459ecd517808473a
LazarYSR <official.lazaryasser@gmail.com>
2024-01-12 22:49:26 +0100

    add Project

---
+++ Prj_RentEase/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Prj_RentEase.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
